ef6bbc5d68d2c94af2e37a619d7064ff6389bea2
dliabenow <dliabenow@gmail.com>
2016-03-13 16:39:21 -0600

    Initial commit
    just getting the basics in place

---
+++ DemoApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+
+namespace DemoApplication
+{
+    public class Program
+    {
+        // https://projecteuler.net/problem=52
+
+        // It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+        // Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+
+        static void Main(string[] args)
+        {
+            // stop at anything greated ther int.MaxValue as this would cause OverflowException
+            for (uint i = 1; i < int.MaxValue; i++)
+            {
+                if (PermutedMultiples.IsMatching(i))
+                {
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ DemoApplicationTest/PermutedMultiples_UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoApplicationTest
+{
+    [TestClass]
+    public class PermutedMultiples_UnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void IsMatching_Negative_Integer_Throws_Exception_TestMethod()
+        {
+                
+        }
+
+    }
+}
