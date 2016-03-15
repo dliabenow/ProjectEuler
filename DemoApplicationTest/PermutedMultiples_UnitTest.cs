3b19f71b5c920dec60d81f874f3d003c08832664
dliabenow <dliabenow@gmail.com>
2016-03-15 11:44:53 -0600

    added final test method

--- DemoApplicationTest/PermutedMultiples_UnitTest.cs
+++ DemoApplicationTest/PermutedMultiples_UnitTest.cs
@@ -30,5 +30,23 @@ namespace DemoApplicationTest
             Assert.AreEqual(result, false, "The expected false result was not received for the number 30.");
         }
 
+        [TestMethod]
+        public void IsMatching_Returns_125874_on_Execution_TestMethod()
+        {
+            const uint expectedResult = 125874;
+            uint smallestMatchingInt = 0;
+
+            for (uint i = 1; i < PermutedMultiples.MaxThreshold; i++)
+            {
+                if (PermutedMultiples.IsMatching(i))
+                {
+                    smallestMatchingInt = i;
+                    break;
+                }
+            }
+
+            Assert.AreEqual(expectedResult, smallestMatchingInt, "Did not generate the expected result '125874'");
+        }
+
     }
 }
